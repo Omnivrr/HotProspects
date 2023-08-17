2cf617368e3af76b42b4c17dcdb23b42e9f6b528
Omnivrr <Omnivrr@gmail.com>
2023-08-17 23:16:32 +0100

    Commence

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,23 +7,10 @@
 
 import SwiftUI
 
-@MainActor class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
 struct EditView: View {
-    @EnvironmentObject var user: User
 
     var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-
-    var body: some View {
-        Text(user.name)
+        Text("Name")
     }
 }
 
