95eed4fc9f19163acd80af19dedb0ba80031afdc
Omnivrr <Omnivrr@gmail.com>
2023-08-17 23:57:57 +0100

    Catching errors

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,16 +7,31 @@
 
 import SwiftUI
 
-struct EditView: View {
+struct ContentView: View {
+    @State private var output = ""
 
     var body: some View {
-        Text("Name")
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+
+    func fetchReadings() async {
+        do {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            output = "Found \(readings.count) readings"
+        } catch {
+            print("Download error")
+        }
     }
 }
 
 
 struct EditView_Previews: PreviewProvider {
     static var previews: some View {
-        EditView()
+        ContentView()
     }
 }
