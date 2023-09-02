563f3c717c34b06394f9fad3c3dff2d2a7724606
Omnivrr <Omnivrr@gmail.com>
2023-09-02 23:10:00 +0100

    Adding Packages

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -4,38 +4,27 @@
 //
 //  Created by Bukhari Sani on 16/08/2023.
 //
-
+import SamplePackage
 import SwiftUI
-import UserNotifications
+
 
 struct ContentView: View {
+    let possibleNumbers = Array(1...60)
+    
     var body: some View {
-        VStack {
-            Button ("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            Button("Schedule Notifications") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the fish"
-                content.subtitle = "They should be hungry right now"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text(results)
     }
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
+    }
+
 }
-struct ContentView_Previews: PreviewProvider {
+
+
+struct EditView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
