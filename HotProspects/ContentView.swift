4a9041a9cd517344234b564d6e8528faab06acf0
Omnivrr <Omnivrr@gmail.com>
2023-09-04 23:55:52 +0100

    Building tab bars

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -4,21 +4,14 @@
 //
 //  Created by Bukhari Sani on 16/08/2023.
 //
-import SamplePackage
 import SwiftUI
 
 
 struct ContentView: View {
-    let possibleNumbers = Array(1...60)
+
     
     var body: some View {
-        Text(results)
-    }
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
+        Text("Hello AbdulQadeer")
     }
 
 }
