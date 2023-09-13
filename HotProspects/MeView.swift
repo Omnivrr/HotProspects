5bc1b69b460ed0d9fc7e2a0293789672dfa34482
Omnivrr <Omnivrr@gmail.com>
2023-09-13 23:57:10 +0100

    Added a QR Code filter

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -4,12 +4,31 @@
 //
 //  Created by Bukhari Sani on 04/09/2023.
 //
-
+import CoreImage
 import SwiftUI
 
 struct MeView: View {
+    @State private var name = "Bukhari"
+    @State private var emailAddress = "akbukharisani@gmail.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title3)
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title3)
+            }
+            .navigationTitle("Kabala")
+        }
     }
 }
 
