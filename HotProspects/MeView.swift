2addaabae0728d7511763783953792b9ac6a448c
Omnivrr <Omnivrr@gmail.com>
2023-09-15 23:33:46 +0100

    QR Code generator working fine

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -27,10 +27,30 @@ struct MeView: View {
                 TextField("Email address", text: $emailAddress)
                     .textContentType(.emailAddress)
                     .font(.title3)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
             }
             .navigationTitle("Kabala")
         }
     }
+    
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        
+        if let outputImage = filter.outputImage {
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            return UIImage(cgImage: cgimg)
+            
+        }
+    }
+    
+    return UIImage(systemName: "xmark.circle") ?? UIImage()
+}
 }
 
 struct MeView_Previews: PreviewProvider {
